68e2dcfcccc64de56f6a828e1cb85fe416e1cfba
DanielYU <Daniel@server>
2012-07-31 23:46:13 +0800

    Now under asource control

---
+++ SpaceVikingGL1/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  SpaceVikingGL1
+//
+//  Created by Daniel yu on 12-7-31.
+//  Copyright 广工 2012年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SpaceVikingGL1/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SpaceVikingGL1
+//
+//  Created by Daniel yu on 12-7-31.
+//  Copyright 广工 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
